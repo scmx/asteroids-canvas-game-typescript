748a8733c46741b7b1d2770a641a5b3358d1ad7f
Albert Arvidsson <albert.arvidsson@gmail.com>
2023-10-25 19:55:46 +0200

    Fix broken audio urls locally with double slash

--- src/audio.ts
+++ src/audio.ts
@@ -1,24 +1,24 @@
 const EFFECTS = ["projectile", "enemy-hit2", "game-over", "buzz"] as const;
-type Effect = typeof EFFECTS[number];
+type Effect = (typeof EFFECTS)[number];
 
 const AUDIO = new Map(
   EFFECTS.map((name) => {
     const audio = document.createElement("audio");
-    audio.src = `${import.meta.env.BASE_URL}/audio/${name}.mp3`;
+    audio.src = `${import.meta.env.BASE_URL}audio/${name}.mp3`;
     audio.volume = 0.2;
     return [name, { audio }];
-  })
+  }),
 );
 
 export const handleAudio = () => {
-  let enabled = false
+  let enabled = false;
   return {
     start() {
-      enabled = true
+      enabled = true;
     },
 
     play(name: Effect) {
-      if (!enabled) return
+      if (!enabled) return;
       let { audio } = AUDIO.get(name)!;
       audio.play();
     },
